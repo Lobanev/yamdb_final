4c61f2cba9504e393c8df9c0d69d3522da750db6
Александр Лобанёв <Djakomo-89@mail.ru>
2023-03-31 16:45:02 +0300

    14

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,12 +1,11 @@
 import datetime as dt
 
+from django.contrib.auth import get_user_model
+from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from rest_framework.serializers import ValidationError
-from django.contrib.auth import get_user_model
-from django.contrib.auth.validators import UnicodeUsernameValidator
-
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.validators import username_validator
 
